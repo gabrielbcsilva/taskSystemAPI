f7e596726d353967fe7a315e34093cfcbae8fef5
Gabriel Bruno <gabrielbcsilva1@gmail.com>
2023-02-10 22:32:01 -0300

    feat: Adding Request Routes(Post, Update, Delete)

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using taskSystemAPI.Models;
+using taskSystemAPI.Repositories.Interfaces;
 
 namespace taskSystemAPI.Controllers
 {
@@ -11,9 +12,37 @@ namespace taskSystemAPI.Controllers
     [Route("api/[controller]")]
     public class UserController : ControllerBase
     {
+        private readonly IUserRepository _userRepository;
+        
+        public UserController(IUserRepository userRepository){
+            _userRepository = userRepository;
+        }
+        
         [HttpGet]
-        public ActionResult<List<UserModel>> listUsers(){
-            return Ok();
+        public async Task<ActionResult<List<UserModel>>>  ListUsers(){
+           List<UserModel> users = await _userRepository.GetAll();
+           return Ok(users);
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<UserModel>>  GetById(long id){
+           UserModel user = await _userRepository.GetUserById(id);
+           return Ok(user);
+        }
+        [HttpPost]
+        public async Task<ActionResult<UserModel>>  Save([FromBody] UserModel userModel){
+           UserModel user = await _userRepository.Save(userModel);
+           return Ok(user);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<UserModel>>  Update([FromBody] UserModel userModel,long id){
+           UserModel user = await _userRepository.Update(userModel,id);
+           return Ok(user);
+        }
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<UserModel>>  Delete(long id){
+           Boolean deleted = await _userRepository.DeleteById(id);
+           return Ok(deleted);
         }
     }
 }
